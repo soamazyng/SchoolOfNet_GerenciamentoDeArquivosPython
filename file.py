caf85556088a711293983d666023d5971efec9ba
Jay Benedicto <Jay@MacBook-Pro-de-Jay.local>
2018-04-16 22:15:20 -0300

    escrevendo em um arquivo

--- file.py
+++ file.py
@@ -1,7 +1,14 @@
-from test import test_file
+import os
 
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
-def teste():
-    print(test_file.minha)
-
-teste()
+file = open(BASE_PATH + '/_file_test.dat', 'w') # w = escrever
+file.write('JAY')
+file.write('\n')
+file.write('\n')
+file.write('\n')
+file.write('\n')
+file.write('\n')
+file.write('JAY2')
+file.close()
+os.rename(BASE_PATH + '/_file_test.dat', BASE_PATH + '/_file2_test.dat')
